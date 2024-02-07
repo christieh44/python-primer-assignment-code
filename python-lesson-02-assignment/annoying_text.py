3558432e029b0521f24824ac8e5feb1edb9a2657
christieh44 <christie.ho@agriculture.vic.gov.au>
2024-02-08 08:04:19 +1100

    python primer lesson 1 code submission

---
+++ python-lesson-02-assignment/annoying_text.py
@@ -0,0 +1,14 @@
+
+# Creates variable original_text and assign string
+original_text = "Hamburgers and pizza! "
+
+# Creates variable shouting_text which turns string into upper case
+shouting_text = original_text.upper()
+
+# Creates variable annoying_text which repeats shouting_text 10 times
+annoying_text = 10*shouting_text
+
+# Creates 2 reports to give output about annoying_text - prints annoying_text and gives number of characters
+print(f"My annoying text is {annoying_text}\n")
+
+print(f"It has {len(annoying_text)} characters.")
